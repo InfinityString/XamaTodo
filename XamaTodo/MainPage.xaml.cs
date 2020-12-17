dc99d3d66f69f2684c9d22c93c3f362660f6cc25
InfinityString <ogbunnamdi@gmail.com>
2020-12-17 10:23:58 +0100

    Add project files.

---
+++ XamaTodo/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamaTodo.Models;
+
+namespace XamaTodo
+{
+    public partial class MainPage : ContentPage
+    {
+     public ObservableCollection<ToDo> AllToDo { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            var db = new SQLiteConnection(SqlConnection.SqlPath);
+
+            db.CreateTable<ToDo>();
+
+            AllToDo = new ObservableCollection<ToDo>(db.Table<ToDo>().ToList());
+            BindingContext = this;
+
+        }
+
+        private void Editor_ChildAdded(object sender, ElementEventArgs e)
+        {
+
+        }
+       
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TextBox1.Text)) {
+                await DisplayAlert("Error", "Enter todo item", "OK");
+                return;
+            }
+            var db = new SQLiteConnection(SqlConnection.SqlPath);           
+
+            var todo = new ToDo()
+            {
+                Done = false,
+                TodoText = TextBox1.Text,
+                TodoTime = TimeBox.Time.ToString()
+
+            };
+            db.Insert(todo);
+            AllToDo.Add(todo);
+           
+
+        }
+
+       
+        protected override void OnDisappearing()
+        {
+            var db = new SQLiteConnection(SqlConnection.SqlPath);
+            base.OnDisappearing();
+            db.UpdateAll(AllToDo);
+        }
+        private void SwipeItem_Invoked(object sender, EventArgs e)
+        {
+            var db = new SQLiteConnection(SqlConnection.SqlPath);
+            SwipeItem item = sender as SwipeItem;
+            ToDo model = item.BindingContext as ToDo;            
+            AllToDo.Remove(model);
+            db.Delete(model);
+
+        }
+    }
+}
---
+++ XamaTodo/Models/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace XamaTodo.Models
+{
+    [Table("Todo")]
+    public class ToDo
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        [MaxLength(500)]
+        public string TodoText { get; set; }
+        [MaxLength(10)]
+        public string TodoTime { get; set; }
+
+        public bool Done { get; set; }
+
+    }
+}
